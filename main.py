287b929029410d94f67a35202e1db694ed1b0656
DenisStenklerTrailer <77147548+DenisStenklerTrailer@users.noreply.github.com>
2023-02-06 11:40:39 +0100

    App created in streamlit

---
+++ main.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import requests
+
+st.set_page_config(layout="wide")
+
+api_key = "kKMlCR9znrFDsTDphaG754Fb32cSeBgDTpIxlObo"
+url = "https://api.nasa.gov/planetary/apod?api_key=kKMlCR9znrFDsTDphaG754Fb32cSeBgDTpIxlObo"
+
+# Get the request data as a dictionary
+request = requests.get(url)
+content = request.json()
+
+# Extraction of the data
+date = content["date"]
+image = content["hdurl"]
+explanation = content["explanation"]
+
+##### STREAMLIT #####
+# st.set_page_config(layout="wide")
+
+st.title("ASTRONOMY PICTURE OF THE DAY")
+st.header(date)
+st.image(image, width=400)
+st.write(explanation)
